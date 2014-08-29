667ce6efe51c7bc3fa821cff1e0c5798c89b7905
Brian Wickman <wickman@twitter.com>
2014-08-29 09:10:07 -0700

    Add debugging info to travis_setup.sh

--- third_party/travis_setup.sh
+++ third_party/travis_setup.sh
@@ -2,9 +2,12 @@
 
 THIRD_PARTY=$(dirname $0)
 
+echo "THIRD_PARTY: $THIRD_PARTY"
+
 git clone https://github.com/tornadoweb/tornado $THIRD_PARTY/tornado
 pushd $THIRD_PARTY/tornado
   python setup.py sdist
   cp dist/*.tar.gz $THIRD_PARTY
 popd
 
+ls -laR
