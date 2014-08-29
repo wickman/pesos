adf24cd72f986490c676b271e966538315a2f5aa
Brian Wickman <wickman@twitter.com>
2014-08-29 09:13:30 -0700

    Possibly fix travis.

--- third_party/travis_setup.sh
+++ third_party/travis_setup.sh
@@ -7,7 +7,7 @@ echo "THIRD_PARTY: $THIRD_PARTY"
 git clone https://github.com/tornadoweb/tornado $THIRD_PARTY/tornado
 pushd $THIRD_PARTY/tornado
   python setup.py sdist
-  cp dist/*.tar.gz $THIRD_PARTY
 popd
 
-ls -laR
+mv $THIRD_PARTY/tornado/dist/*.tar.gz $THIRD_PARTY
+rm -rf $THIRD_PARTY/tornado
